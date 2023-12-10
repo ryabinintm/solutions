754268d3dbfe135a1fed03cd2812dfb94f72b945
Alexander Ryabinin <ryabinintm@yandex.ru>
2023-12-10 16:54:13 +0300

    product except self

--- LeetCode/solutions.cpp
+++ LeetCode/solutions.cpp
@@ -26,3 +26,15 @@ int max_profit(vector<int>& prices) {
   }
   return maxprof;
 }
+
+vector<int> product_except_self(vector<int> &nums) {
+  vector<int>res(nums.size(), 1);
+  for(int i = 1; i < nums.size(); i++)
+    res[i] = res[i-1] * nums[i-1];
+  int right = 1;
+  for(int i = nums.size() - 1; i >= 0; i--){
+    res[i] *= right;
+    right *= nums[i];
+  }
+  return res;
+}
